28fe122b70b3a429fccf60d6dec43773755257d7
ninestar9552 <ninestar9552@gmail.com>
2026-01-25 23:46:57 +0900

    feat: WKWebView 기본 로딩 구현
    - WKWebView 추가 및 AutoLayout 설정                                                                                       - 로컬 HTML 파일 로딩 기능 구현

--- webview/ViewController.swift
+++ webview/ViewController.swift
@@ -6,14 +6,51 @@
 //
 
 import UIKit
+import WebKit
 
 class ViewController: UIViewController {
-
+    
+    // MARK: - Properties
+    
+    private var webView: WKWebView!
+    
+    // MARK: - Lifecycle
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        setupWebView()
+        loadLocalHTML()
     }
+    
+    // MARK: - Setup
+    
+    private func setupWebView() {
+        let configuration = WKWebViewConfiguration()
+        
+        webView = WKWebView(frame: .zero, configuration: configuration)
+        webView.translatesAutoresizingMaskIntoConstraints = false
+        
+        view.addSubview(webView)
+        
+        NSLayoutConstraint.activate([
+            webView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            webView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            webView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            webView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
+        ])
+    }
+    
+    // MARK: - Load HTML
+    
+    private func loadLocalHTML() {
+        guard let htmlURL = Bundle.main.url(
+            forResource: "index",
+            withExtension: "html"
+        ) else {
+            print("❌ index.html 못 찾음")
+            return
+        }
 
-
+        webView.loadFileURL(htmlURL, allowingReadAccessTo: htmlURL.deletingLastPathComponent())
+    }
 }
-
