82791096b00a30de16bce74277ee123140db41e6
ninestar9552 <ninestra9552@gmail.com>
2026-01-25 21:17:20 +0900

    Initial Commit

---
+++ webviewTests/webviewTests.swift
@@ -0,0 +1,17 @@
+//
+//  webviewTests.swift
+//  webviewTests
+//
+//  Created by 차순혁 on 1/25/26.
+//
+
+import Testing
+@testable import webview
+
+struct webviewTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
